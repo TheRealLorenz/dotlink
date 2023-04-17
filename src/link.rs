0e51e75fd04e9be482f56967baad5154729274e2
TheRealLorenz <lollobellina@gmail.com>
2023-04-17 13:36:45 +0200

    Update config file structure

--- src/link.rs
+++ src/link.rs
@@ -1,3 +1,4 @@
+use colored::*;
 use std::{fmt, fs, io, path::PathBuf};
 
 pub struct LinkEntry {
@@ -16,22 +17,36 @@ impl fmt::Display for LinkEntry {
     }
 }
 
-impl LinkEntry {
-    pub fn symlink(&self) -> Result<(), io::Error> {
-        let from = &self.from;
-        let to = &self.to;
-
-        if let Err(e) = std::os::unix::fs::symlink(from, to) {
-            if e.kind() == std::io::ErrorKind::AlreadyExists
-                && fs::symlink_metadata(to).unwrap().is_symlink()
-                && &fs::read_link(to).unwrap() == from
-            {
-                return Ok(());
-            }
-
-            return Err(e);
+fn symlink(from: &PathBuf, to: &PathBuf) -> Result<(), io::Error> {
+    if let Err(e) = std::os::unix::fs::symlink(from, to) {
+        if e.kind() == std::io::ErrorKind::AlreadyExists
+            && fs::symlink_metadata(to).unwrap().is_symlink()
+            && &fs::read_link(to).unwrap() == from
+        {
+            return Ok(());
         }
 
-        Ok(())
+        return Err(e);
+    }
+
+    Ok(())
+}
+
+impl LinkEntry {
+    pub fn symlink(&self, dry_run: bool) {
+        print!("Linking {}: ", self);
+
+        if dry_run {
+            println!("{}", "dry".yellow().bold());
+            return;
+        }
+
+        match symlink(&self.to, &self.from) {
+            Ok(_) => println!("{}", "✓".green().bold()),
+            Err(e) => {
+                println!("{}", "X".red().bold());
+                eprintln!("  - {e}")
+            }
+        }
     }
 }
--- src/preset.rs
+++ src/preset.rs
@@ -1,58 +1,59 @@
-use crate::{expand, link};
-use colored::*;
+use crate::{expand, link::LinkEntry};
 use serde::Deserialize;
 use std::{collections::HashMap, fs, path::Path};
 
 pub mod error;
 
-#[derive(Deserialize)]
-pub struct Presets {
-    presets: HashMap<String, Preset>,
+#[derive(Deserialize, Debug)]
+struct SingleEntry {
+    name: String,
+    to: String,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Preset {
-    links: Vec<Entry>,
+struct MultipleEntry {
+    names: Vec<String>,
     to: String,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(untagged)]
 enum Entry {
-    SimpleEntry(SimpleEntry),
-    CustomEntry(CustomEntry),
+    Single(SingleEntry),
+    Multiple(MultipleEntry),
 }
 
-type SimpleEntry = String;
+#[derive(Deserialize)]
+pub struct Preset(Vec<Entry>);
 
-#[derive(Deserialize, Debug)]
-struct CustomEntry {
-    name: String,
-    to: String,
-}
+impl Preset {
+    pub fn apply(&self, from_dir: &Path, dry_run: bool) -> Result<(), expand::ExpandError> {
+        for entry in &self.0 {
+            match entry {
+                Entry::Single(single_entry) => {
+                    let from = from_dir.join(&single_entry.name);
+                    let to =
+                        expand::expand_tilde(Path::new(&single_entry.to))?.join(&single_entry.name);
 
-impl Entry {
-    fn to_link_entry(
-        &self,
-        from_dir: &Path,
-        default_to: &String,
-    ) -> Result<link::LinkEntry, expand::ExpandError> {
-        match self {
-            Entry::SimpleEntry(name) => {
-                let from = from_dir.join(name);
-                let to = expand::expand_tilde(Path::new(default_to))?.join(name);
+                    LinkEntry { from, to }.symlink(dry_run);
+                }
+                Entry::Multiple(multiple_entry) => {
+                    for name in &multiple_entry.names {
+                        let from = from_dir.join(name);
+                        let to = expand::expand_tilde(Path::new(&multiple_entry.to))?.join(name);
 
-                Ok(link::LinkEntry { from, to })
-            }
-            Entry::CustomEntry(a) => {
-                let from = from_dir.join(&a.name);
-                let to = expand::expand_tilde(Path::new(&a.to))?;
-
-                Ok(link::LinkEntry { from, to })
+                        LinkEntry { from, to }.symlink(dry_run);
+                    }
+                }
             }
         }
+
+        Ok(())
     }
 }
+
+#[derive(Deserialize)]
+pub struct Presets(HashMap<String, Preset>);
 
 impl Presets {
     pub fn from_file(path: &dyn AsRef<Path>) -> Result<Self, error::LoadError> {
@@ -63,35 +64,10 @@ impl Presets {
     }
 
     pub fn names(&self) -> Vec<String> {
-        self.presets.keys().cloned().collect::<Vec<String>>()
+        self.0.keys().cloned().collect::<Vec<String>>()
     }
 
     pub fn get(&self, preset_name: &String) -> Option<&Preset> {
-        self.presets.get(preset_name)
-    }
-}
-
-impl Preset {
-    pub fn apply(&self, from_dir: &Path, dry_run: bool) -> Result<(), expand::ExpandError> {
-        for entry in &self.links {
-            let link_entry = entry.to_link_entry(from_dir, &self.to)?;
-
-            print!("Linking {}: ", link_entry);
-
-            if dry_run {
-                println!("{}", "dry".yellow().bold());
-                continue;
-            }
-
-            match link_entry.symlink() {
-                Ok(_) => println!("{}", "✓".green().bold()),
-                Err(e) => {
-                    println!("{}", "X".red().bold());
-                    eprintln!("  - {e}")
-                }
-            }
-        }
-
-        Ok(())
+        self.0.get(preset_name)
     }
 }
